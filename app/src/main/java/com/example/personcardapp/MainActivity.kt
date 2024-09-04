03b9aef1cba6c441500976e594e02ed263088378
Mohamad Farhoud <Mohamadfarhoud@hotmail.com>
2024-09-04 21:07:46 +0200

    Add CardList to MainActivity

--- app/src/main/java/com/example/personcardapp/MainActivity.kt
+++ app/src/main/java/com/example/personcardapp/MainActivity.kt
@@ -4,13 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.personcardapp.components.CardList
+import com.example.personcardapp.repository.getAllPersons
 import com.example.personcardapp.ui.theme.PersonCardAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,30 +20,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             PersonCardAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Box(modifier = Modifier.padding(innerPadding)) {
+                        CardList(persons = getAllPersons())
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-
-
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PersonCardAppTheme {
-        Greeting("Android")
     }
 }
